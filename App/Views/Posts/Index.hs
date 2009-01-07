b804a122dea0f3a93f3eb8888d42fb9aeb6f749e
Adolfo <builes.adolfo@gmail.com>
2009-01-07 17:18:43 +0000

    Posts are saved and show, small bug in the view "Index"

--- App/Views/Posts/Index.hs
+++ App/Views/Posts/Index.hs
@@ -1,1 +1,16 @@
-markup =  <h1> Hello Word </h1>
+markup =  <div>
+              <h1> A poor-man's Blog</h1>
+              <% do l <-  getViewDataValue_u "posts-list" ::View [(String,String,String)] 
+                    mapM indexItem l
+              %>
+                                
+          </div>
+
+indexItem (i,t,b) = return $ cdata $ unlines $
+                   ["<div style='padding: 0pt 5px;'>"
+                   ," <a href=\"/Page/Show/" ++ i ++"\">"
+                   ,"  "++ t
+                   ," </a>"
+                   ,"<p>" ++ b ++ "</p>"
+                   ,"</div>"
+                   ]
--- tmp/compiled/App/Views/Posts/Index.hs
+++ tmp/compiled/App/Views/Posts/Index.hs
@@ -6,5 +6,20 @@ import Turbinado.View.Helpers
 import Control.Monad.Trans
 
 markup :: View XML
-markup =  <h1> Hello Word </h1>-- SPLIT HERE
+markup =  <div>
+              <h1> A poor-man's Blog</h1>
+              <% do l <-  getViewDataValue_u "posts-list" ::View [(String,String,String)] 
+                    mapM indexItem l
+              %>
+                                
+          </div>
+
+indexItem (i,t,b) = return $ cdata $ unlines $
+                   ["<div style='padding: 0pt 5px;'>"
+                   ," <a href=\"/Page/Show/" ++ i ++"\">"
+                   ,"  "++ t
+                   ," </a>"
+                   ,"<p>" ++ b ++ "</p>"
+                   ,"</div>"
+                   ]-- SPLIT HERE
 
