aefd47183c5da6231a7271deaaddbaaf9a34fd51
Adolfo <builes.adolfo@gmail.com>
2009-01-08 17:19:32 +0000

    Bug found in setViewDataValue

--- App/Controllers/Posts.hs
+++ App/Controllers/Posts.hs
@@ -12,22 +12,20 @@ new =  do setViewDataValue "save-url" ("Create")
 create :: Controller ()
 create = do body_    <-  getParam_u "content"
             title_   <-  getParam_u "title"
-            d        <-  getClockTime
-            id'      <-  insert(Posts {_id=Nothing , body = body_ , title = title_, createdAt = d }) True 
+            d        <-  lift getClockTime
+            id'      <-  insert(Posts {_id=Nothing , body = body_ , title = title_, createdAt = (Just d) }) True 
             redirectTo $ "/Posts/Show/" ++ Prelude.show(fromJust(id')) 
 
 
 show :: Controller ()
 show =  do id' <-getSetting_u "id" :: Controller String
            p <- find (read id' :: Int64) :: Controller Posts
+           setViewDataValue "post-date" (createdAt p)
            setViewDataValue "post-title" (title p)
            setViewDataValue "post-content" (body p)
            
 index :: Controller ()
 index = do posts <- findAll :: Controller [Posts]           
-           setViewDataValue "posts-list" $ map (\p -> (Prelude.show(fromJust(_id p)),title p , body p,genDate(createdAt p))) posts
+           setViewDataValue "posts-list" $ map (\p -> (Prelude.show(fromJust(_id p)),title p , body p)) posts
 
 
-genDate :: ClockTime -> String
-genDate p = do d <-(toCalendarTime p)
-               (Prelude.show(ctMonth d) ++ " - " ++ Prelude.show(ctDay d) ++ " - " ++ Prelude.show(ctYear d))
