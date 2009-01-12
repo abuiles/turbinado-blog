da29e582ccd1c1574fbc899a92b9bfc7a10100dc
Adolfo <builes.adolfo@gmail.com>
2009-01-12 00:49:01 +0000

    Adding Comments

---
+++ App/Controllers/Comments.hs
@@ -0,0 +1,11 @@
+import App.Models.Comments as CommentsModel
+import qualified Network.URI as URI
+
+
+create :: Controller ()
+create = do id' <-getSetting_u "id" :: Controller String
+            body_ <- getParam_u "content"
+            author_ <- getParam_u "author"
+            let pId = (Prelude.read id')::Int64
+            CommentsModel.insert Comments{author = author_ ,body = body_ ,commentId =Nothing,postId = pId} False
+            redirectTo $ "/Posts/Show/"++id'
