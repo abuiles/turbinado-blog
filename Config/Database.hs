2c35a9e79e0afcf2df52fd2659f81547a3ac866e
Adolfo <builes.adolfo@gmail.com>
2009-02-25 20:57:29 +0000

    Updating

--- Config/Database.hs
+++ Config/Database.hs
@@ -13,6 +13,6 @@ import Turbinado.Environment.Types
 ----------------------------------------------------------------
 -- databaseConnection :: Maybe (IO Connection)
 -- databaseConnection = Nothing
-databaseConnection = Just $ connectPostgreSQL "host=localhost dbname=blog user=turbinado password=turbinado"
+databaseConnection = Jst $ connectPostgreSQL "host=localhost dbname=blog user=turbinado password=turbinado"
 
 
